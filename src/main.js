8176b8ebd3139bfd34819ac7fff0859f92492424
rctsdev <rctsdev@gmail.com>
2025-05-05 11:17:27 -0300

    feat: ativando funções

--- src/main.js
+++ src/main.js
@@ -9,18 +9,18 @@ async function main() {
     try {
         // Autentica e baixa o CSV
         const auth = await authenticate();
-        return
+
         const arquivoCSV = await downloadCSVFile(auth);
         
         // Processa o CSV
         const dadosEscala = await processarCSV(arquivoCSV);
         
         // Gera o PDF
-        const pdfPath = path.join(__dirname, '../uploads', 'escala_voluntarios.pdf');
+        const pdfPath = path.join(__dirname, './uploads', 'escala_voluntarios.pdf');
         await gerarPDF(dadosEscala, pdfPath);
         
         // Envia o PDF por e-mail
-        // await enviarEmail(pdfPath);
+        await enviarEmail(pdfPath);
         
         // Envia o PDF pelo WhatsApp
         // await enviarWhatsApp(pdfPath);
